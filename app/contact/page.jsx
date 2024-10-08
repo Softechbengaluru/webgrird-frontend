5501b081bd3d247652e4603a7236b7036bae156e
jayanth <jayanths8899@gmail.com>
2024-10-08 21:34:29 +0530

    fix

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white mt-50">
-      <div className="mt-5 p-8 md:px-12 rounded-lg w-full max-w-2xl ">
-        <h1 className="text-6xl font-bold mb-10 text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white mt-12 px-4">
+      <div className="mt-5 p-8 md:px-12 rounded-lg w-full max-w-md md:max-w-2xl">
+        <h1 className="text-4xl md:text-6xl font-bold mb-6 text-center">
           Become a client
         </h1>
-        <p className="text-gray-600 text-left text-sm mb-20">
+        <p className="text-gray-600 text-left text-sm mb-10 md:mb-20">
           Our clients get the best results when they have our team dedicated to
           their business <br />
           for extended periods of time. This is why we are looking for ongoing
@@ -22,49 +22,55 @@ const Contact = () => {
           <input
             type="text"
             placeholder="First name"
-            className="w-full mb-10 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[18px] leading-[1.5] min-h-[60px] px-4 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500" // Applied custom styles using Tailwind CSS
+            className="w-full mb-6 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[16px] leading-[1.5] min-h-[50px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="text"
             placeholder="Last name"
-            className="w-full mb-10 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[18px] leading-[1.5] min-h-[60px] px-4 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500" // Applied custom styles using Tailwind CSS
+            className="w-full mb-6 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[16px] leading-[1.5] min-h-[50px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="text"
             placeholder="Job title"
-            className="w-full mb-10 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[18px] leading-[1.5] min-h-[60px] px-4 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500" // Applied custom styles using Tailwind CSS
+            className="w-full mb-6 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[16px] leading-[1.5] min-h-[50px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="email"
             placeholder="email@company.com"
-            className="w-full mb-10 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[18px] leading-[1.5] min-h-[60px] px-4 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500" // Applied custom styles using Tailwind CSS
+            className="w-full mb-6 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[16px] leading-[1.5] min-h-[50px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
           <input
             type="text"
             placeholder="How did you find Ramotion"
-            className="w-full mb-10 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[18px] leading-[1.5] min-h-[60px] px-4 py-4 focus:outline-none focus:ring-2 focus:ring-blue-500" // Applied custom styles using Tailwind CSS
+            className="w-full mb-6 bg-[#f2f2f2] border border-[#f2f2f2] rounded-[8px] text-[#262626] text-[16px] leading-[1.5] min-h-[50px] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <div className="mb-10 text-center text-gray-400">
+          <div className="mb-6 text-center text-gray-400 text-xs md:text-sm">
             By filling in the form, you agree to our Privacy Policy, including
             our cookie use.
           </div>
 
           <div className="text-center">
-            <button className="bg-gray-300 text-gray-600 px-24 py-6 rounded-full text-lg hover:bg-gray-400">
+            <button className="bg-gray-300 text-gray-600 px-8 py-4 rounded-full text-lg hover:bg-gray-400 md:px-24 md:py-6">
               Get in Touch
             </button>
           </div>
         </form>
 
-        <div className="mt-6 text-center">
-          <a href="#" className="text-grey-600 hover:underline text-sm mr-4">
+        <div className="mt-4 text-center">
+          <a
+            href="#"
+            className="text-gray-600 hover:underline text-xs md:text-sm mr-4"
+          >
             Press Inquiries
           </a>
-          <span className="text-gray-600 mr-4">→</span>{" "}
-          <a href="#" className="text-grey-600 hover:underline text-sm mr-4">
+          <span className="text-gray-600 mr-4">→</span>
+          <a
+            href="#"
+            className="text-gray-600 hover:underline text-xs md:text-sm mr-4"
+          >
             Join Ramotion
           </a>
-          <span className="text-gray-600">→</span>{" "}
+          <span className="text-gray-600">→</span>
         </div>
       </div>
     </div>
