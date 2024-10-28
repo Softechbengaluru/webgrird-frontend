ad98c69da958e318614ec9151e4c53165d545cec
kartik <kartik200421@gmail.com>
2024-10-28 16:47:43 +0530

    Added Routing To Contact Page

--- app/app-design/page.jsx
+++ app/app-design/page.jsx
@@ -359,13 +359,15 @@ const page = () => {
             <Faq />
           </div>
           <div className="flex justify-center mt-10 sm:mt-28 mb-10 sm:mb-10">
-            <button
-              type="submit"
-              className="flex px-6 sm:px-11 sm:py-5 2xl:px-28 lg:px-20 lg:h-20 2xl:text-3xl xl:text-2xl xl:h-20 xl:px-20 2xl:h-24 justify-center gap-2 items-center mx-auto shadow-xl font-normal bg-[#262626] backdrop-blur-md text-white isolation-auto border-[#262626] before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-white hover:text-[#262626] before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 py-2 overflow-hidden border-2 rounded-full group"
-            >
-              Shall we Talk?
-              <ArrowForwardIcon />
-            </button>
+          <Link href="/contact">
+          <button
+            type="submit"
+            className="flex px-6 sm:px-11 sm:py-5 2xl:px-28 lg:px-20 lg:h-20 2xl:text-3xl xl:text-2xl xl:h-20 xl:px-20 2xl:h-24 justify-center gap-2 items-center mx-auto shadow-xl font-normal bg-[#262626] backdrop-blur-md text-white isolation-auto border-[#262626] before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-white hover:text-[#262626] before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 py-2 overflow-hidden border-2 rounded-full group"
+          >
+            Shall we Talk?
+            <ArrowForwardIcon />
+          </button>
+          </Link>
           </div>
         </section>
       </div>
